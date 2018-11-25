37743c3b663de3da7d06cfc4faa8bfb2b8f291e9
TheQuengineer <qmanthomas@gmail.com>
2018-11-25 14:13:10 -0800

    Added a way to get all the sport names and their associated images for easy lookup by model

--- app.py
+++ app.py
@@ -6,9 +6,26 @@ from flask import Flask
 from flask import Flask, abort, request, render_template
 from flask import Response
 import logging
+import glob
 
 
 app = Flask(__name__)
+
+def get_image_data():
+    """
+    This function is designed to get all the possible images and their names that
+    the model will be able to show via the sports template. The model will can use
+    this function to search for the image and its corresponding sport name in order to
+    display it to the user.
+
+
+    Returns:
+    <list> [(sport, user_image)...]
+    """
+    user_images = [i.replace('static/img/', "") for i in glob.glob('static/img/*.png')]
+    sports = [i.replace('.png', "").capitalize().replace("_", " ") for i in user_images]
+    data = list(zip(sports, user_images))
+    return data
 
 
 @app.route('/', methods=['GET', 'POST'])
